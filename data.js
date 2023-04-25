db0d55afa4daffa9dfeb02bdba672414329ceeae
Yudina Tamila <79252896+Taaamilaaa@users.noreply.github.com>
2023-04-25 20:25:48 +0300

    previous markup and the beginning of the slider

---
+++ data.js
@@ -0,0 +1,27 @@
+export const imgPath = [
+    {
+    name: "Kembang Flower Mantap",
+    path: "./images/hero_img_1.png"
+    },
+        {
+    name: "Rahasia Plant",
+    path: "./images/hero_img_2.jpg"
+    },
+            {
+    name: "Monster",
+    path: "./images/hero_img_3.jpg"
+    },
+            {
+    name: "Heather or indoor ivy",
+    path: "./images/hero_img_4.jpg"
+    },
+            {
+     name: "Miniature rose",
+     path: "./images/hero_img_5.jpg"
+     },
+             {
+     name: "Kaktus Plant",
+     path: "./images/hero_img_6.jpg"
+ },
+    
+]
---
+++ js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    heroImg: document.querySelector('.hero__img'),
+    heroTitle: document.querySelector('.hero__title'),
+    previousName: document.querySelector('.previousName'),
+    nextName: document.querySelector('.nextName'),
+    
+    heroBtnNext: document.querySelector('.nextBtn'),
+    heroBtnPrev: document.querySelector('.prevBtn'),
+}
